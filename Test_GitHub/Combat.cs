81acf882840633da14c51e7e76fc3bb14f20a6db
Dieter Schwarz <Dieter@Culsu.De>
2018-09-05 12:51:27 +0200

    Test nach Erstellung Prototyp

---
+++ Test_GitHub/Combat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Test_GitHub
+{
+    class Combat : INotifyPropertyChanged
+
+    {
+        private string name;
+        private string ort;
+
+
+        #region Konstruktoren
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged("Name");
+
+            }
+        }
+        public string Ort
+        {
+            get { return ort; }
+            set
+            {
+                this.ort = value;
+                OnPropertyChanged("Ort");
+            }
+        }
+        #endregion
+
+
+
+        public Combat(string name, string ort)
+        {
+            this.Name = name;
+            this.Ort = ort;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ Test_GitHub/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_GitHub
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+        private Combat combat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            combat = new Combat("Midgard", "Staufenberg");
+
+            this.DataContext = combat;
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            int rest = counter % 3;
+
+            switch (rest)
+            {
+                case 0:
+                    combat.Name = "Doppelkopf";
+                    combat.Ort = "Lollar";
+                    break;
+                case 1:
+                    combat.Name = "Risiko";
+                    combat.Ort = "Gießen";
+                    break;
+                case 2:
+                    combat.Name = "Midgard 1880";
+                    combat.Ort = "Staufenberg, Lumdatalstrasse 4";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
